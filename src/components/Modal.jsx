58c50d87f4d6526cd689dde0f7c1ca6fbb376cbf
hoyoungsin <hoyoungsin@gmail.com>
2023-05-08 15:15:04 -0400

    fixed bug with info not updating

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,18 +1,4 @@
-import { useEffect, useState } from "react";
-import { getPokeInfo } from "../services/pokemon.js";
-
-export default function Modal({poke}) {
-    const [pokeInfo, setPokeInfo] = useState("")
-
-    const fetchPokeInfo = async () => {
-        const info = await getPokeInfo(poke.id)
-        setPokeInfo(info.flavor_text_entries[0].flavor_text)
-    } 
-    
-    useEffect(()=>{
-        fetchPokeInfo()
-    }, [])
-
+export default function Modal({poke, pokeInfo}) {
     let pokeName = poke.name.charAt(0).toUpperCase() + poke.name.slice(1);
     return (
         <div className="modal__content">
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getPokeInfo } from "../services/pokemon.js";
 import Pokemon from "../components/Pokemon"
 import Modal from "../components/Modal";
 
 export default function Home({poke}) {
   const [ pokeId, setPokeId ] = useState("")
+  const [ pokeInfo, setPokeInfo ] = useState("")
 
+  const fetchPokeInfo = async (id) => {
+    const info = await getPokeInfo(id)
+    setPokeInfo(info.flavor_text_entries[0].flavor_text)
+  } 
+  
   const handleClick = (id) => {
     setPokeId(id)
+    fetchPokeInfo(id)
   }
 
   return (
@@ -16,7 +24,7 @@ export default function Home({poke}) {
         {poke.map(pokemon => <Pokemon handleClick={handleClick} key={pokemon.id} id={pokemon.id} poke={pokemon} />)}
       </div>
       <div id="info-modal" className="modal">
-        {pokeId ? <Modal poke={poke.find(pokemon => pokemon.id === pokeId)} /> : <></>}
+        {pokeId ? <Modal poke={poke.find(pokemon => pokemon.id === pokeId)} pokeInfo={pokeInfo} /> : <></>}
       </div>
     </>
   )
