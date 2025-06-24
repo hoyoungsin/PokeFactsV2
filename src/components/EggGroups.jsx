56a017de6d63832538492955684e87a198f7d5b9
Hoyoung Sin <hoyoungsin@gmail.com>
2025-06-24 12:28:14 -0400

    No egg group update, Abilities CSS update.

--- src/components/EggGroups.jsx
+++ src/components/EggGroups.jsx
@@ -1,9 +1,9 @@
 export default function EggGroups({eggGroup}) {
     let groupName = eggGroup.charAt(0).toUpperCase() + eggGroup.slice(1);
-    console.log(eggGroup)
+    // console.log(eggGroup)
     return (
         <p className="eggGroup">
-            {groupName}
+            {groupName==="No-eggs" ? "No Egg Group" : groupName}            
         </p>
     )
 }
