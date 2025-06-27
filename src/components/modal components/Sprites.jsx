8af6cae3ff866917724ec0ebfb430c72afac0734
Hoyoung Sin <hoyoungsin@gmail.com>
2025-06-27 00:02:21 -0400

    Moved sprite to modal component to allow future work on selectable sprites by generation

---
+++ src/components/modal components/Sprites.jsx
@@ -0,0 +1,7 @@
+export default function Sprites({sprites}) {
+    return (
+        <div className="spritesContainer">
+            <img className="picture" src={sprites.front_default} alt="default" /> 
+        </div>
+    )
+}
