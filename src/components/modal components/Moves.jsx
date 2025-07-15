e8bbe3a2bddde042dcf75a806f5e7aa84ae12af5
Hoyoung Sin <hoyoungsin@gmail.com>
2025-07-15 16:21:23 -0400

    Added moves container with drop down and states for generation.

--- src/components/modal components/Moves.jsx
+++ src/components/modal components/Moves.jsx
@@ -1,11 +1,49 @@
 import { useState } from "react"
 
-export default function Moves({moves}) {
-    const [ generation, setGeneration ] = useState("Gen I")
+export default function Moves({moveSet}) {
+    const [ showMoves, setShowMoves ] = useState(false)
+    const [ moveGen, setMoveGen ] = useState("Gen I")
 
+    for (let i = 0; i < moveSet.length; i++) {
+        for (let j = 0; j < moveSet[i].version_group_details.length; j++) {
+            if (moveSet[i].version_group_details[j].version_group.name === moveGen) {
+                console.log(moveSet[i].move.name)
+                console.log(moveSet[i].version_group_details[j].level_learned_at)
+                console.log(moveSet[i].version_group_details[j].move_learn_method.name)
+            }
+        }
+    }
+
+    const handleGen = (gen) => {
+        setMoveGen(gen)
+    }
+
+    const handleButton = () => {
+        if (showMoves === false) {
+            setShowMoves(true)
+        } else {
+            setShowMoves(false)
+        }
+    }
+
+    const Moves = () => {
+        return (
+            <div>
+                <p>
+                    Generation
+                    <select onChange={(e) => handleGen(e.target.value)}>
+                        <option value="Gen I">Gen I</option>
+                    </select>
+                </p>
+            </div>
+        )
+    }
+
+    console.log(moveSet)
     return (
         <div className="movesContainer">
-            Moves
+            <button className="movesdropbtn" onClick={handleButton}>Moves</button>
+            {showMoves ? <Moves/>: <></>}
         </div>
     )
 }
