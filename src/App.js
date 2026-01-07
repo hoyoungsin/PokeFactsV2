326e4e9103f2f1e8b4dd9806adc5504477b7a7ec
Hoyoung Sin <hoyoungsin@gmail.com>
2026-01-07 11:58:40 -0500

    Updated pokeAmount/slider logic and added useCallback to stabilize pokemon generation.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
-import { useState, useEffect } from "react"
-import Home from "./screens/Home.jsx"
+import { useCallback, useState, useEffect } from "react";
+import Home from "./screens/Home.jsx";
 import { getPokemon } from './services/pokemon';
 
 function App() {
   const [ pokemon, setPokemon ] = useState([])
-  const [ pokeAmount, setPokeAmount ] = useState(70)
+  const [ pokeAmount, setPokeAmount ] = useState(70) //Applied amount
+  const [ sliderAmount, setSliderAmount ] = useState(70) //Slider amount (draft)
   const [ genAmount, setGenAmount ] = useState(151)
   const [ genTotal, setGenTotal ] = useState(151)
   const [ isLoading, setIsLoading ] = useState(true)
 
-  const generatePokemon = async () => {
+  const generatePokemon = useCallback(async () => {
+    // useCallback needed since the function is passed onto child.
+    setIsLoading(true)
     const poke = [];
     let pokeIDs = [];
     while(poke.length < pokeAmount) {
@@ -23,17 +26,20 @@ function App() {
     }
     setPokemon(poke)
     setIsLoading(false);
-  }
+  }, [pokeAmount, genAmount, genTotal]);
 
   useEffect(() => {
     generatePokemon();
-  },[genAmount, genTotal])
+  }, [generatePokemon])
 
   return (
     <>
       <Home
         isLoading={isLoading}
+        setIsLoading={setIsLoading}
         poke={pokemon}
+        sliderAmount={sliderAmount}
+        setSliderAmount={setSliderAmount}
         pokeAmount={pokeAmount}
         setPokeAmount={setPokeAmount}
         generatePokemon={generatePokemon}
